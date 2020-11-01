a937913c8f0042b910399036bbbdcbd39c8c4efe
Will Keeling <will@zifferent.com>
2020-11-01 14:49:06 +0000

    Create barebones project structure, app

---
+++ pyrite/__main__.py
@@ -0,0 +1,13 @@
+"""This module acts as the entry point for running the application."""
+
+
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    author="Will Keeling",
+    author_email='will@zifferent.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="A lightweight text editor written in Python using Tkinter",
+    install_requires=[],
+    license="MIT",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='pyrite',
+    name='pyrite',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    setup_requires=[],
+    test_suite='pytest',
+    tests_require=['pytest'],
+    url='https://github.com/wkeeling/pyrite',
+    version='0.0.1',
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['pyrite=pyrite.__main__:main']
+    },
+)
