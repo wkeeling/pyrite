1030b52b321dcc26d1b892b11b86c88ae167976f
Will Keeling <will@zifferent.com>
2020-11-05 12:21:28 +0000

    Use regular dictionary, add tests

--- pyrite/config/settings.py
+++ pyrite/config/settings.py
@@ -2,12 +2,12 @@
 
 The application ships with default settings that can be overridden on a per-user
 basis. Per-user settings are stored in the file .pyrite.settings in a user's
-home folder. Overriding is done automatically when the settings are loaded.
+home folder.
 
 To load the settings, clients should call the `initialise()` function which will
 expose them via the global variable `settings`.
 """
-from collections import ChainMap
+import logging
 from io import BytesIO
 from pathlib import Path
 from pkgutil import get_data
@@ -16,14 +16,21 @@ import yaml
 
 SETTINGS_FILENAME = '.pyrite.settings'
 
-# Global variable that holds the initialised per-user settings
-settings: ChainMap = ChainMap()
+log = logging.getLogger(__name__)
+
+# These are the merged user + default settings
+settings: dict = {}
+
+default_settings: dict = {}
 
 
 def initialise():
-    default_settings = yaml.load(
+    """Load the default settings, applying any user overrides, and expose them
+    via the `settings` global.
+    """
+    default_settings.update(yaml.load(
         BytesIO(get_data(__package__, '.pyrite.defaults')), Loader=yaml.SafeLoader
-    )
+    ))
 
     settings_path = Path('~', SETTINGS_FILENAME).expanduser()
 
@@ -32,5 +39,48 @@ def initialise():
     else:
         user_settings = {}
 
-    settings.maps = [user_settings, default_settings]
+    settings.update(default_settings)
+    settings.update(user_settings)
 
+    if not user_settings:
+        # Write out the user settings file on first load
+        save()
+
+
+def get_boolean(name: str) -> bool:
+        val = settings[name]
+
+        if val is True or val is False:
+            return val
+
+        log.warning(f'Invalid boolean value: {val}')
+
+        return default_settings[name]
+
+
+def get_int(name: str) -> int:
+    val = settings[name]
+
+    if isinstance(val, int):
+        return val
+
+    log.warning(f'Invalid integer value: {val}')
+
+    return default_settings[name]
+
+
+def get_float(name: str) -> float:
+    val = settings[name]
+
+    if isinstance(val, float):
+        return val
+
+    log.warning(f'Invalid float value: {val}')
+
+    return default_settings[name]
+
+
+def save():
+    """Save the current settings."""
+    settings_path = Path('~', SETTINGS_FILENAME).expanduser()
+    yaml.dump(settings, settings_path)
--- tests/config/test_settings.py
+++ tests/config/test_settings.py
@@ -1,26 +1,123 @@
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
 from pyrite.config import settings
 
 
-class TestSettings:
+class TestInitialise:
 
     @pytest.fixture
-    def user_settings(self):
-        user_settings = Path('~', settings.SETTINGS_FILENAME).expanduser()
-        user_settings.write_text('tab_size: 2')
-        yield
-        user_settings.unlink()
+    def yaml(self):
+        with patch('pyrite.config.settings.yaml') as mock_yaml:
+            with patch('pyrite.config.settings.Path'):
+                yield mock_yaml
 
-    def test_initialise_no_user_settings(self):
+    @pytest.fixture
+    def save(self):
+        with patch('pyrite.config.settings.save') as mock_save:
+            yield mock_save
+
+    def test_not_initialised(self):
+        assert len(settings.settings) == 0
+
+    def test_initialise_no_user_settings(self, yaml, save):
+        yaml.load.side_effect = [
+            {'tab_size': 4}, {}
+        ]
         settings.initialise()
 
         assert settings.settings['tab_size'] == 4
+        save.assert_called_once_with()
 
-    @pytest.mark.usefixtures('user_settings')
-    def test_initialise_with_user_settings(self):
+    def test_initialise_with_user_settings(self, yaml, save):
+        yaml.load.side_effect = [
+            {'tab_size': 4}, {'tab_size': 2}
+        ]
         settings.initialise()
 
         assert settings.settings['tab_size'] == 2
+        assert save.call_count == 0
+
+
+class TestSave:
+
+    @pytest.fixture
+    def yaml(self):
+        with patch('pyrite.config.settings.yaml') as mock_yaml:
+            yield mock_yaml
+
+    def test_save(self, yaml):
+        settings.settings = {'test_attr: yes'}
+
+        settings.save()
+
+        yaml.dump.assert_called_once_with(
+            settings.settings, Path('~', settings.SETTINGS_FILENAME).expanduser()
+        )
+
+
+class TestGetters:
+
+    @pytest.fixture
+    def yaml(self):
+        with patch('pyrite.config.settings.yaml') as mock_yaml:
+            with patch('pyrite.config.settings.Path'):
+                yield mock_yaml
+
+    @pytest.fixture
+    def log(self):
+        with patch('pyrite.config.settings.log') as mock_log:
+            yield mock_log
+
+    def test_get_boolean(self, yaml):
+        yaml.load.side_effect = [
+            {'test_bool': True}, {}
+        ]
+        settings.initialise()
+
+        assert settings.get_boolean('test_bool') is True
+
+    def test_get_boolean_invalid(self, yaml, log):
+        yaml.load.side_effect = [
+            {'test_bool': True}, {'test_bool': 'foo'}
+        ]
+        settings.initialise()
+
+        assert settings.get_boolean('test_bool') is True
+        log.warning.assert_called()
+
+    def test_get_int(self, yaml):
+        yaml.load.side_effect = [
+            {'test_int': 5}, {}
+        ]
+        settings.initialise()
+
+        assert settings.get_int('test_int') == 5
+
+    def test_get_int_invalid(self, yaml, log):
+        yaml.load.side_effect = [
+            {'test_int': 5}, {'test_int': 'foo'}
+        ]
+        settings.initialise()
+
+        assert settings.get_int('test_int') == 5
+        log.warning.assert_called()
+
+    def test_get_float(self, yaml):
+        yaml.load.side_effect = [
+            {'test_float': 5.0}, {}
+        ]
+        settings.initialise()
+
+        assert settings.get_float('test_float') == 5.0
+
+    def test_get_float_invalid(self, yaml, log):
+        yaml.load.side_effect = [
+            {'test_float': 5.0}, {'test_float': 'foo'}
+        ]
+        settings.initialise()
+
+        assert settings.get_float('test_float') == 5.0
+        log.warning.assert_called()
