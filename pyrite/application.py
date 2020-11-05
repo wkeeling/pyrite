416d6aeb2d90b3f4b9fc04ba6b7d1902529b061b
Will Keeling <will@zifferent.com>
2020-11-05 22:30:34 +0000

    Use correct method signature

--- pyrite/application.py
+++ pyrite/application.py
@@ -15,7 +15,7 @@ THEMES = dict(
 
 def run():
     root = ThemedTk(theme=settings['theme'])
-    settings.add_listener('theme', lambda v: root.set_theme(THEMES[v]))
+    settings.on_save(lambda v: root.set_theme(THEMES[v]))
     label = ttk.Label(master=root, text='Hello')
     label.pack()
     ttk.Button(root, text="Quit", command=root.destroy).pack()
