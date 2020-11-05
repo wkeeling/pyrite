2ce6f1ff5335d9044f451e5553388124d9c13522
Will Keeling <will@zifferent.com>
2020-11-05 22:25:23 +0000

    Refactor to application.run()

--- pyrite/__main__.py
+++ pyrite/__main__.py
@@ -1,18 +1,8 @@
 """This module acts as the entry point for running the application."""
-
-from tkinter import ttk
-
-from ttkthemes import ThemedTk
-
-
-def main():
-    root = ThemedTk(theme='equilux')
-    label = ttk.Label(master=root, text='Hello')
-    label.pack()
-    ttk.Button(root, text="Quit", command=root.destroy).pack()
-
-    root.mainloop()
+from pyrite import application
+from pyrite import settings
 
 
 if __name__ == '__main__':
-    main()
+    settings.initialise()
+    application.run()
---
+++ pyrite/application.py
@@ -0,0 +1,23 @@
+import logging
+from tkinter import ttk
+
+from ttkthemes import ThemedTk
+
+from pyrite import settings
+
+log = logging.getLogger(__name__)
+
+THEMES = dict(
+    dark='equilux',
+    light='arc',
+)
+
+
+def run():
+    root = ThemedTk(theme=settings['theme'])
+    settings.add_listener('theme', lambda v: root.set_theme(THEMES[v]))
+    label = ttk.Label(master=root, text='Hello')
+    label.pack()
+    ttk.Button(root, text="Quit", command=root.destroy).pack()
+
+    root.mainloop()
